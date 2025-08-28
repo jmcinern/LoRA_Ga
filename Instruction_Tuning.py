b3fd9721043c49bd89e20834d842d3b15e100cb0
joseph <jmcinern@tcd.ie>
2025-08-28 11:11:56 +0100

    test mem

---
+++ Instruction_Tuning.py
@@ -0,0 +1,12 @@
+# load model from https://huggingface.co/jmcinern/qwen3-8b-base-cpt
+from transformers import DataCollatorForLanguageModeling, AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments, TrainerCallback, TrainerControl
+import torch
+model_name = "jmcinern/qwen3-8b-base-cpt"
+cache_path = "./cache"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    cache_dir=cache_path,
+    trust_remote_code=True, 
+    torch_dtype=torch.float16
+)
